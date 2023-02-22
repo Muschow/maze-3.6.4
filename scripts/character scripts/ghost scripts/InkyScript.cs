10f6aa5ecac0aeeabd249600a4e18ce2bc87218f
Muschow <shafichowdhury16@gmail.com>
2023-02-22 15:06:03 +0000

    added stackoverflow link

--- scripts/character scripts/ghost scripts/InkyScript.cs
+++ scripts/character scripts/ghost scripts/InkyScript.cs
@@ -10,6 +10,8 @@ public class InkyScript : GhostScript
         Array values = Enum.GetValues(typeof(algo));
         Random random = new Random();
         algo randomAlgo = (algo)values.GetValue(random.Next(values.Length)); //puts all the values in algo enum into an array and chooses a random one
+        //https://stackoverflow.com/questions/3132126/how-do-i-select-a-random-value-from-an-enumeration
+        //adapted code to get random value from enum from here
 
         searchingAlgo = randomAlgo; //blue uses a random algorithm to pathfind
     }
